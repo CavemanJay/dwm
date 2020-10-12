e699b8fcc2a88a44d93afe527af01ea7efb63642
Jay <Jordan.Cuevas@precisecal.com>
2020-10-11 23:14:46 -0400

    Applied systray patch

--- config.def.h
+++ config.def.h
@@ -4,6 +4,10 @@
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char scratchpadname[]  = "scratchpad";
@@ -84,12 +88,14 @@ static const char *termcmd[]  = { "alacritty", NULL };
 static const int gapDelta = 3;
 
 #include "shiftview.c"
+#include <X11/XF86keysym.h>
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	// Dmenu prompts
 	{ ALT,                       	XK_r,      spawn,          SHCMD("dmenu_run -i -p 'Run: '") },
 	{ ALT,                       	XK_space,  spawn,          SHCMD("j4-dmenu-desktop --no-generic --dmenu=\"dmenu -i -p 'Launch: '\" --term='alacritty'") },
-	{ MODKEY,             			XK_x, 	   spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
+
+	// Layout management
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -98,9 +104,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+
+	// Layout selection
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -108,29 +114,46 @@ static Key keys[] = {
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	// { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	// { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+
+	// Monitor management
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+
+	// Layout navigation
 	{ MODKEY,                       XK_n, shiftview,       	   {.i = +1 } },
 	{ MODKEY,                       XK_b, shiftview,       	   {.i = -1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -gapDelta } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +gapDelta } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
-	// TAGKEYS(                        XK_6,                      5)
-	// TAGKEYS(                        XK_7,                      6)
-	// TAGKEYS(                        XK_8,                      7)
-	// TAGKEYS(                        XK_9,                      8)
+
+	// Gap management
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -gapDelta } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +gapDelta } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+
+	// DWM management
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
+
+	// Media keys
+	{ 0, XF86XK_MonBrightnessUp, 	   		   spawn,          SHCMD("xbacklight -inc 5") },
+	{ 0, XF86XK_MonBrightnessDown, 	   		   spawn,          SHCMD("xbacklight -dec 5") },
+	{ 0, XF86XK_AudioMute, 	   		   		   spawn,          SHCMD("$HOME/scripts/audio/volume.zsh mute") },
+	{ 0, XF86XK_AudioRaiseVolume, 	   		   spawn,          SHCMD("$HOME/scripts/audio/volume.zsh up") },
+	{ 0, XF86XK_AudioLowerVolume, 	   		   spawn,          SHCMD("$HOME/scripts/audio/volume.zsh down") },
+
+	// Applications
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_x,  	   togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,             			XK_x, 	   spawn,          {.v = termcmd } },
+	{ MODKEY,             			XK_e, 	   spawn,          SHCMD("alacritty -e ranger") },
 };
 
 /* button definitions */
